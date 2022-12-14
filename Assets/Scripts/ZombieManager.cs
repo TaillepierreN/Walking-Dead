039219deb47ecdf54e7e7fad13cf7d139cbc9ce6
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2022-12-14 17:17:58 +0100

    zombie animation/attack

--- Assets/Scripts/ZombieManager.cs
+++ Assets/Scripts/ZombieManager.cs
@@ -8,18 +8,31 @@ public class ZombieManager : MonoBehaviour
 
     NavMeshAgent _agent;
     GameObject _target;
+    Animator _animator;
     // Start is called before the first frame update
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
         _target = GameObject.FindGameObjectWithTag("Player");
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        var distance = Vector3.Distance(_target.transform.position, gameObject.transform.position);
+        if(distance> 1.0f)
+        {
         _agent.SetDestination(_target.transform.position);
+        _animator.SetBool("IsAttacking",false);
+
+        }
+        else{
+
+        _animator.SetBool("IsAttacking",true);
+        }
+
         
     }
 }
