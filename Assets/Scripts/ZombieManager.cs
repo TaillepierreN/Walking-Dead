f7ddfe1bcfa4dd3a47123d5c56e49d71abcb7fa9
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2022-12-20 12:15:52 +0100

    added unsync animation/ audiomixer / crosshair

--- Assets/Scripts/ZombieManager.cs
+++ Assets/Scripts/ZombieManager.cs
@@ -9,12 +9,24 @@ public class ZombieManager : MonoBehaviour
     NavMeshAgent _agent;
     GameObject _target;
     Animator _animator;
+    AudioSource _audioSource;
+    Coroutine zombieSound;
     // Start is called before the first frame update
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
         _target = GameObject.FindGameObjectWithTag("Player");
         _animator = GetComponent<Animator>();
+        _audioSource =  gameObject.GetComponent<AudioSource>();
+        _animator.SetFloat("Offset", Random.Range(0.0f, 1.0f));
+        //int aleatoire = Random.Range(1,100);
+        //if (aleatoire >90)
+        //{
+        //  _audioSource.Play();
+        //}
+        var zombieTime = Random.Range(1,50);
+        StartCoroutine(ZombieMoan(zombieTime));
+        
     }
 
     // Update is called once per frame
@@ -36,6 +48,21 @@ public class ZombieManager : MonoBehaviour
         _animator.SetBool("IsAttacking",true);
         }
 
+
+
         
+    }
+    private IEnumerator ZombieMoan(int timer)
+    {
+        yield return new WaitForSeconds(timer);
+        if(!_audioSource.isPlaying)
+        {
+        _audioSource.Play();
+        }
+        var zombieTime = Random.Range(1,50);
+        StartCoroutine(ZombieMoan(zombieTime));
+
+        
+
     }
 }
